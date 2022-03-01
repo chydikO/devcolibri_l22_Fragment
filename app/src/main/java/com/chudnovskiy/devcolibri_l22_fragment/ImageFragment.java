09a425440d88decc2bc293488d6ac1f5abb4cffa
Chudnovskiy.O.W520 <oleg.chudnovskiy@gmail.com>
2022-03-01 17:32:44 +0200

    Android Урок 22. Знакомимся с Fragment-ами

---
+++ app/src/main/java/com/chudnovskiy/devcolibri_l22_fragment/ImageFragment.java
@@ -0,0 +1,17 @@
+package com.chudnovskiy.devcolibri_l22_fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ImageFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.image_layout, container, false);
+    }
+}
